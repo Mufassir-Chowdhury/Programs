a110eb9545f834a8b0eab0ff8ca0212b21717f91
Mufassir Ahmad Chowdhury <mac22214u@gmail.com>
2020-12-25 11:41:43 +0600

    site

--- Skill developement/Tailwind/tailwind.config.js
+++ Skill developement/Tailwind/tailwind.config.js
@@ -1,16 +1,40 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'sl': '880px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontWeight: {
         semibold: 600,
+      },
+      fontFamily: {
+        bangla: ['kalpurush']
+      },      
+      colors: {
+        fbGray: '#242526',
+        fbdark: '#18191A',
+        fbdiv: '#3A3B3C',
       }
     },
   },
   variants: {
-    extend: {},
-    backgroundColor: ['responsive', 'focus', 'hover', 'active'],
+    extend: {
+      visibility: ['dark'],
+      display: ['dark'],
+      stroke: ['dark'],
+      fill: ['dark'],
+      opacity: ['dark'],
+      backgroundOpacity: ['dark'],
+    },
+    backgroundColor: ['responsive', 'focus', 'hover', 'active', 'dark'],
+    backgroundImage: ['dark'],
   },
   plugins: [],
 }
